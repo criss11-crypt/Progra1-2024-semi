4e4f3bb3ad58beafdfd71f19f4e6a057a4643542
Cristofer Enmanuel Machado Guillen <USTR168322@ugb.edu.sv>
2024-07-24 09:49:32 -0600

    determinar el proyecto de una serie de numeros

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,22 @@ namespace MiPrimerProyecto
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {//Prioridad de los operadores arigmeticos.
+            //ejercicios obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5,4,6,8,9};//32
+            int suma = 0;
+            foreach (int num in serie) {
+                suma = suma + num;
+            }
+            double prom = suma / serie.Length;
+            Console.WriteLine("la suma es: {0}, el promedio {1}", suma, prom);
+            //pausa.
+            Console.ReadLine();
+                
+
+
+
         }
+     
     }
 }
