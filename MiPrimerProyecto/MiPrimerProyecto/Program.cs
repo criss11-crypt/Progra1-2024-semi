1627296dab1911ae9e08cce414b26d836dfa6a73
Cristofer Enmanuel Machado Guillen <USTR168322@ugb.edu.sv>
2024-08-13 23:53:59 -0600

    uso de matriz bidimencional

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -5,22 +5,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiPrimerProyecto
+namespace miPrimerProyecto
 {
     internal class Program
     {
         static void Main(string[] args)
-        {            //Uso de matrices.
+        {
             //Uso de matrices.
-            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            Console.WriteLine("El numero de la posicion 3 es {0}", numeros[3]);
-            for (int i = 0; i < numeros.Length; i++)
-            {
-                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+                {
+                int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
+
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    Console.WriteLine("Indice: {0} = {1}", i, numeros[i]);
+                }
+
+                int[,] matriz = new int[,] {
+                  { 5, 4, 5, 2, 1 },
+                  { 1, 2, 8, 9, 3 }
+        };
+
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.Write("{0} ", matriz[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                // Pausa
+                Console.ReadKey();
             }
-            //Pausa
-            Console.ReadKey();
         }
     }
 }
+
 
